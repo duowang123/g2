fa918405aa87027b024e887fa26ff6a0b7a1714c
LAPTOP-2VA9KBTD\mark <237395167@qq.com>
2020-08-25 15:54:41 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import G2 from '@antv/g2/dist/g2.min.js';
+// import G2 from '@antv/g2/dist/g2.min.js';
 import store from './store'
 import router from './router'
 // import api from './https/index'
@@ -14,7 +14,7 @@ Vue.use(ElementUI)
 
 
 // 挂载全局的g2
-Vue.prototype.$G2 = G2
+// Vue.prototype.$G2 = G2
 
 Vue.config.productionTip = false
 new Vue({
--- src/views/404.vue
+++ src/views/404.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wscn-http404-container">
     <div class="text">您访问的页面可能迷路了......</div>
-    <el-button class="btn" type="info" block @click="goHome()">返回首页</el-button>
+    <el-button type="primary" class="btn" @click="goHome()">返回首页</el-button>
   </div>
 </template>
 
@@ -24,18 +24,10 @@ export default {
 <style lang='scss' scoped>
 .wscn-http404-container {
   margin: 15px;
-  .text {
-    font-size: 16px;
-    font-weight: 500;
-    line-height: 22px;
-    color: rgba(102, 102, 102, 1);
-    margin-top: 44px;
-    margin-bottom: 33px;
-  }
-  .icon {
-    width: 226px;
-    height: 126px;
-    margin-bottom: 25px;
+  padding-top: 200px;
+  text-align: center;
+  .btn {
+    margin: 15px auto;
   }
 }
 </style>
